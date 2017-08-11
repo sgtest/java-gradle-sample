e6ad398cb5c1b7f7c182ea37dfea3f10bcf6fdfb
Beyang Liu <beyang@sourcegraph.com>
2017-08-11 10:30:09 -0700

    add third-party artifactory repository

---
+++ src/main/java/mypkg/SourcegraphSimpleUser.java
@@ -0,0 +1,12 @@
+package mypkg;
+
+import com.sourcegraph.simple.Map;
+import com.sourcegraph.simple.MyInterface;
+
+/**
+ * Created by beyang on 8/11/17.
+ */
+public class SourcegraphSimpleUser {
+    private Map map;
+    private MyInterface myInterface;
+}
